b462471398c3f451594a211cfea744a1fc433275
SheltonXiao <pi620903@163.com>
2023-05-14 16:35:28 +0800

    update ignore

---
+++ scripts/config.py
@@ -0,0 +1,39 @@
+# -*- coding:utf-8 -*-
+'''
+@File    :   config.py
+@Time    :   2023/05/11 12:07:34
+@Author  :   SheltonXiao
+@Version :   1.0
+@Contact :   pi620903@163.com
+@Desc    :   None
+@License :
+
+    (c) Copyright 2022 SheltonXiao
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or(at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY;without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+    GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.If not, see < https: //www.gnu.org/licenses/>.
+'''
+
+# here put the import lib
+import os
+
+# device config
+OPENAI_API_KEY = "" #"填入openAI API key"  
+
+# vector storage config
+PINECONE_API_KEY = ""
+PINECONE_ENV = ""
+PINE_CONE_DB_NAME = ""
+
+if __name__ == '__main__':
+    pass
